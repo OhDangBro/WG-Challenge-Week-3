0c5c2b930e01fe7b1e035568b4e489b5f1ad8dea
Will <103394466+OhDangBro@users.noreply.github.com>
2022-05-25 16:43:04 -0400

    finalized working code, making generator function

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-// Assignment code here
 
-var charSets = {
 
-  theNumbers : "1234567890",
-  lowerCase : "abcdefghijklmnopqrstuvwxyz",
-  upperCase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  theSymbols : "!@#$%%^&*",
+var numbers = ["0", "1", "2", "3", "4", "4", "5", "6", "7", "8", "9", "10"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "`"];
 
-}
+var passwordOptions = [];
+var finalPassword = [];
 
-var length = ""
+// var length = ""
 var lowerCasePrompt = ""
 var upperCasePrompt = ""
 var symbolsPrompt = ""
 var numbersPrompt = ""
 
+var choices = [
 
-
-
+]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -25,61 +24,69 @@ var generateBtn = document.querySelector("#generate");
 function allPrompts() {
   length = Number(prompt("Pick a password length between 8 and 124"))
 
-    if (length < 8 || length > 124) {
-      alert("You picked an invalid number, please pick a number between 8 and 124")
-      allPrompts() 
-
-    }else {
-      console.log("plzz")
-    } 
-
-
-
-
-  // lowerCasePrompt = window.prompt( "Do you want lowercase  included in password click okay if yes")
-
-  // upperCasePrompt = window.prompt( "Do you want upper case included in password click okay if yes")
-
-  // symbolsPrompt = window.prompt( "Do you want symbols included in password click okay if yes")
-  
-  // numbersPrompt = window.prompt( "Do you want numbers included in password click okay if yes")
-
-
-  
-
-
-  
-
-
-
-  // else {
-  //   console.log("u picked rite boiiiii")
-  // }
-
-
-}
-
-function generatepassword() {
-  var passwordValue 
-  for( var i = 0; i < length; i++) {
-    passwordValue += charSets.charAt(
-      Math.floor(Math.random() * charSets.length)
-
-    )   
+  if (length < 8 || length > 124) {
+    alert("You picked an invalid number, please pick a number between 8 and 124")
+    allPrompts()
   }
+
+
+  lowerCasePrompt = confirm("Do you want lowercase  included in password click okay if yes")
+
+  if (lowerCasePrompt) {
+    passwordOptions = passwordOptions.concat(lowerCase)
+  }
+
+  upperCasePrompt = confirm("Do you want upper case included in password click okay if yes")
+
+  if (upperCasePrompt) {
+    passwordOptions = passwordOptions.concat(upperCase)
+  }
+
+  symbolsPrompt = confirm("Do you want symbols included in password click okay if yes")
+  if (symbolsPrompt) {
+    passwordOptions = passwordOptions.concat(symbols)
+  }
+
+  numbersPrompt = confirm("Do you want numbers included in password click okay if yes")
+  if (numbersPrompt) {
+    passwordOptions = passwordOptions.concat(numbers)
+  }
+
+
+  console.log(lowerCasePrompt);
+  console.log(upperCasePrompt);
+  console.log(symbolsPrompt);
+  console.log(numbersPrompt);
+
+  console.log('passwordOptions', passwordOptions)
+
+  for (var i = 0; i < length; i++) {
+    finalPassword.push(getRandom(passwordOptions))
+  }
+  return finalPassword
+
+
+
+
 }
 
+function writePassword() {
+  var password = allPrompts();
+  var passText = document.getElementById('password')
 
+  passText.value = password.join('')
 
-  
+}
 
+function getRandom(passwordOptions) {
+  var randomIndex = Math.floor(Math.random() * passwordOptions.length);
+  var randomElement = passwordOptions[randomIndex]
 
-
-
-
+  return randomElement
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", allPrompts);
+generateBtn.addEventListener("click", writePassword);
 
 
 
